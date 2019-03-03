b513e2b8e3a6d391f9648ff403104f86927e04f2
Chantal S <chantal.shaib@gmail.com>
2019-03-02 19:40:44 -0500

    new file for training/testing

--- GAN_models.py
+++ GAN_models.py
@@ -12,20 +12,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
-# =============================== Load Dataset ============================= #
-
-# Depending on what the data looks like, length/width/height should be adjusted
-# Assuming all augmentation and pre-processing steps are done
-# Training and testing splits remain the same
-
-# length =
-# width =
-# height ='
-
-# Normalize the data (not sure if necessary?)
-# Split into training, validation, and test sets
-
-# ========================= Models and Loss Functions ======================= #
+# ========================= 3D GAN and VAE GAN Models ======================= #
 
 class GAN():
     def __init__(self, learning_rate, noise_dim, in_shape=(128, 128, 128, 1), out_shape=(64, 64, 64)):
---
+++ train.py
@@ -0,0 +1,13 @@
+# =============================== Load Dataset ============================= #
+
+# Depending on what the data looks like, length/width/height should be adjusted
+# Assuming all augmentation and pre-processing steps are done
+# Training and testing splits remain the same
+
+# length =
+# width =
+# height ='
+
+# Normalize the data (not sure if necessary?)
+# Split into training, validation, and test sets
+
